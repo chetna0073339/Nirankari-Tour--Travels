e24858e8720d4cc98efa86a382732c3ccd5a2b02
chetna0073339 <chetnachauhan704@gmail.com>
2026-03-02 15:07:57 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.static(__dirname));
+
+// temporary storage (you can later connect MongoDB)
+let bookings = [];
+
+// Booking API
+app.post("/book", (req, res) => {
+
+    const booking = {
+        name: req.body.name,
+        phone: req.body.phone,
+        service: req.body.service,
+        date: req.body.date
+    };
+
+    bookings.push(booking);
+
+    console.log("New Booking:", booking);
+
+    res.send("Booking saved");
+});
+
+
+// Admin API to get all bookings
+app.get("/admin/bookings", (req, res) => {
+
+    res.json(bookings);
+
+});
+
+
+app.listen(3000, () => {
+
+    console.log("Server running on http://localhost:3000");
+
+});
+
